61a7e45d06268410bfb9d57ad8d57e8171e57dde
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2019-04-01 20:26:28 -0600

    Product Type Data Provider

---
+++ Ventas/resources/data/ProductTypeDataProvider.php
@@ -0,0 +1,16 @@
+<?php
+	include("DataProvider.php");
+	$selectProductTypeColumns = "ID, Name, Comission";
+
+    function GetAllProductTypes(){
+        global $selectProductTypeColumns;
+		$sql = "SELECT ".$selectProductTypeColumns." FROM ProductType;";
+		return SqlSelect($sql);
+    }
+
+	function GetProductType($id){
+		global $selectProductTypeColumns;
+		$sql = "SELECT ".$selectProductTypeColumns." FROM ProductType WHERE ID = ".$id.";";
+		return SqlSelect($sql);
+	}
+?>

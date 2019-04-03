75ea24fb506b67c77be55f4d716b8cbbf01aa6c6
JARP-96 <JARP-96@users.noreply.github.com>
2019-04-03 14:49:59 -0600

    1st Commit
    Uploaded index and login sites (under construction)

---
+++ Ventas/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sistema</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="row" id="navebar">
+        <div class="col-sm-4">Sistema de Ventas</div>
+        <div class="col-sm-4" id="navebar">Jorge Antonio Ramirez Padilla</div>
+        <div class="col-sm-4" id="navebar">03/04/2019</div>
+    </div>
+    <div id="section_1">
+        <div class="container">
+        <div class="d-flex justify-content-sm-center bg-warning mb-5">
+                <div class="col-sm-4 pagination-centered">
+                    <button type="button" class="btn btn-primary">Primary</button>
+                </div>
+                <div class="col-sm-4" align="center">
+                    <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

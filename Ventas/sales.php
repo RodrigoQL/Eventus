6f6e58b92b6ca85cd6345ebfade2f4fdedc86787
JARP-96 <JARP-96@users.noreply.github.com>
2019-04-03 18:49:48 -0600

    aAAAAAAAAAAA
    ASSSSSSSSSSSAAAAAAAAAAAAA

---
+++ Ventas/sales.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Nueva Venta</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <button>+</button>
+    <h1>Nueva Venta<h2>
+    <table>
+        <tr>
+        <th scope="col">dog</th>
+        <th scope="col">cat</th>
+        <th scope="col">horse</th>
+
+        <th scope="col">iguana</th>
+        </tr>
+        <tr>
+        <td>5</td>
+        <td>2</td>
+
+        <td>10</td>
+        <td>500</td>
+        </tr>
+
+        </table>
+        <p> Filtrar por fecha de ingreso del:</p>
+        <input type="date"> al <input type="date">
+        <button type="button" class="btn btn-primary">Comisiones</button>
+</body>
+</html>

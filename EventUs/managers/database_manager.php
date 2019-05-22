909040e27ede86c99fc65b70e5f0977592ca6c20
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2019-05-22 18:31:35 -0500

    Eventus Setup

---
+++ EventUs/managers/database_manager.php
@@ -0,0 +1,42 @@
+<?php
+    $db_servername = "localhost";
+    $db_username = "wwwplust_Eventus";
+    $db_password = "Even7us2019";
+    $db_name = "wwwplust_Eventusdb";
+
+    function sqlSelect($sql){
+        global $db_servername, $db_username, $password, $db_name;
+        $conn = mysqli_connect( $db_servername, $db_username, $db_password, $db_name);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $result = mysqli_query($conn, $sql);
+        while( $row = mysqli_fetch_array($result,MYSQLI_BOTH) ){
+            $rows[] = $row;
+        }
+
+        mysqli_close($conn);
+        return( $rows );
+    }
+
+
+    function sqlInsert($query){
+        global $db_servername, $db_username, $db_password, $db_name;
+        $conn = mysqli_connect( $db_servername, $db_username, $db_password, $db_name);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = mysqli_query($conn, $query);
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            return true;
+        } else {
+            echo "Error: " . $sql . "<br>" . $query . "<br>" .$conn->error;
+            return false;
+        }    
+
+        mysqli_close($conn);
+    }    
+?>

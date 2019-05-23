1a1d907efdfae2d4074d237819a67bc92a5289e2
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2019-05-22 19:48:30 -0500

    v0.1 views

--- EventUs/resources/data/UserDataProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("DataProvider.php");
-
-	function CreateUser($user, $password, $name, $lastname, $email, $phone, $userTypeID){
-		$sql = "INSERT INTO User (Name, Lastname, Username, Password, Email, Phone, UserTypeID) VALUES ('".$name."','".$lastname."','".$user."','".$password."','".$email."','".$phone."',".$userTypeID.");";
-        return sqlInsert($sql);;
-	}
-?>

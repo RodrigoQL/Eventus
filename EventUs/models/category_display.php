dfa14eaae7b28559b3218d7e82e005f3dc81a122
JARP-96 <JARP-96@users.noreply.github.com>
2019-06-06 22:42:41 -0500

    WiP User
    Work in Progress de User view y model

--- EventUs/models/category_display.php
+++ EventUs/models/category_display.php
@@ -6,14 +6,16 @@
 
     $html = '';
     foreach($categories as $row) {
-        $html .= '<div class="category col-6 col-sm-3 col-md-2">';
-        $html .= '<h3 class="category-title">'.$row['Name'].'</h3>';
+        $html .= '<div class="col-md-3 col-sm-4 col-xs-12 single_portfolio_text category">';
         $category_image = $row['ImageUrl'];
         if ($category_image === NULL){
             $category_image = "http://plustexting.com/equipo1/EventUs/assets/img/party.png";
         }
-        $html .= '<img class="category-image" src="'.$category_image.'" style="width:100%;">';
-        $html .= '<button data-index="'.$row['Id'].'" class="btn btn-ok category-button">Seleccionar</button>';
+        $html .= '<img class="category-image" src="'.$category_image.'" style="max-width:100%;">';
+        $html .= '<div class="portfolio_images_overlay text-center ">';
+        $html .= '<h6 class="clrd-font category-title">'.$row['Name'].'</h6>';
+        $html .= '<button data-index="'.$row['Id'].'" class="btn btn-primary category-button">Seleccionar</button>';
+        $html .= '</div>';
         $html .= '</div>';
     }
     
--- EventUs/views/user.php
+++ EventUs/views/user.php
@@ -1,30 +1,44 @@
 <!doctype html>
-    <html>
-        <head>
+<html>
 
-            <title>.:: Eventus ::.</title>
-            <meta charset="utf-8">
-    
-            <!-- RENDERING CORRECTO DEPENDIENDO DEL TAMANO DE LA PANTALLA -->
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-            <!-- LLAMA REFERENCIAS CDN DE BOOTSTRAP Y LIBRERIAS REQUERIDAS POR BOOTSTRAP -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-            <script src="../controllers/user_controller.js"></script>
-        </head>
-        <body class="body">
-            <div id="navbar">
-                <input type="button" id="cart-button" value="Carrito de Compras" />
-            </div>
-            <h1>Selecciona una categoria</h1>
-            <div id="category-list" class="row">
-                Cargando categorías....
-            </div>
-            <br>
-            <div id="business-list">
-            </div>
-        </body>
+<head>
+
+    <title>.:: Eventus ::.</title>
+    <meta charset="utf-8">
+
+    <!-- RENDERING CORRECTO DEPENDIENDO DEL TAMANO DE LA PANTALLA -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- LLAMA REFERENCIAS CDN DE BOOTSTRAP Y LIBRERIAS REQUERIDAS POR BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/user-styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="../controllers/user_controller.js"></script>
+</head>
+
+<body class="body">
+    <nav id="navbar" class="navbar navbar-dark bg-dark justify-content-between">
+        <a class="navbar-brand">EVENTUS</a>
+        <a id="cart-button" class="btn btn-info btn-lg">
+        <span class="glyphicon glyphicon-search"></span> Carrito de Compras
+        </a>
+    </nav>
+    <h1 align="center">Selecciona el tipo de negocio</h1>
+    <div id="category-list" class="row">
+        Cargando categorías....
+    </div>
+    <br>
+    <div id="business-list">
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+    <script src="../assets/js/script.js"></script>
+
+</body>
+
 </html>
